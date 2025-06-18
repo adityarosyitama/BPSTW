6b614f9359728cf62022463003fdf1cd5f1082eb
adityarosyitama <addityarosyyitama2@gmail.com>
2025-06-18 21:55:29 +0700

    Initial commit from Create Next App

---
+++ src/components/SlideInRL.tsx
@@ -0,0 +1,26 @@
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+import { ReactNode } from "react";
+
+interface SlideInProps {
+  children: ReactNode;
+}
+
+const SlideInRL = ({ children }: SlideInProps) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.3 });
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ x: 100, opacity: 0 }}
+      animate={isInView ? { x: 0, opacity: 1 } : { x: 100, opacity: 0 }}
+      transition={{ duration: 1, ease: "easeOut" }}
+      className="text-center text-xl"
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default SlideInRL;
